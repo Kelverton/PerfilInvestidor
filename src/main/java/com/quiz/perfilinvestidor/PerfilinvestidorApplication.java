9c3eca9eb2a08ae7c802d60e5b519f001f613eb9
Thiago Vinicius Santos Almeida <th.vinialmeida@gmail.com>
2025-11-27 20:41:45 -0300

    Feat: First Commit

---
+++ src/main/java/com/quiz/perfilinvestidor/PerfilinvestidorApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.perfilinvestidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilinvestidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilinvestidorApplication.class, args);
+	}
+
+}
